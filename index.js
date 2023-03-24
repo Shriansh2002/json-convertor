9ecc7ecb2e7a3a15b94a46abf66f63d93215f633
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2023-03-24 13:57:46 +0530

    Removed Dev Dependencies

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
-const json2csv = require('json2csv').parse;
-const xml2js = require('xml-js');
-
 function convertToCsv(json) {
-    const csv = json2csv(json);
-    return csv;
+    const separator = ",";
+    let header = "";
+    let rows = "";
+
+    Object.keys(json[0]).forEach(key => {
+        header += key + separator;
+    });
+    header = header.slice(0, -1);
+
+    json.forEach(obj => {
+        let row = "";
+        Object.values(obj).forEach(value => {
+            row += value + separator;
+        });
+        rows += row.slice(0, -1) + "\n";
+    });
+
+    const csvData = header + "\n" + rows;
+    return csvData;
 }
 
-function convertToXml(json) {
-    const options = { compact: true, ignoreComment: true, spaces: 4 };
-    const xml = xml2js.js2xml(json, options);
-    return xml;
+function convertToXml(jsonData) {
+    let xmlData = "";
+
+    const jsonToXmlRec = (node, obj) => {
+        let xml = "";
+        for (const prop in obj) {
+            if (obj.hasOwnProperty(prop)) {
+                if (typeof obj[prop] === "object") {
+                    xml += "<" + prop + ">";
+                    xml += jsonToXmlRec(node + "." + prop, obj[prop]);
+                    xml += "</" + prop + ">";
+                } else {
+                    xml += "<" + prop + ">" + obj[prop] + "</" + prop + ">";
+                }
+            }
+        }
+        return xml;
+    };
+
+    for (const prop in jsonData) {
+        if (jsonData.hasOwnProperty(prop)) {
+            xmlData += "<" + prop + ">";
+            xmlData += jsonToXmlRec(prop, jsonData[prop]);
+            xmlData += "</" + prop + ">";
+        }
+    }
+
+    return xmlData;
 }
 
 module.exports = { convertToCsv, convertToXml };
